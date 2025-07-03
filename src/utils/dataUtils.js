2690e47fdc00bf78d86effc85afd547a0ff37369
Roveref <francoisrovere@gmail.com>
2025-07-03 10:45:51 +0200

    Update dataUtils.js

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -200,7 +200,7 @@ export const calculateRevenueWithSegmentLogic = (item, showNetRevenue = false) =
   }
 
   // If no specific Operations allocation, return full revenue
-  return baseRevenue;
+  return 0;
 };
 
 /**
